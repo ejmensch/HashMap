ca9c56d4cc566dcb23e05441f2eeae8dbb37d294
ethan <ethan@LAPTOP-O9MCJS4B>
2021-05-02 00:08:10 -0400

    added more primes, only makes 2 of the suess files?

--- HashMap/mainHash.cpp
+++ HashMap/mainHash.cpp
@@ -12,10 +12,10 @@ int main() {
 	makeSeuss voice3("DrSeuss.txt","Seussout3.txt",true,false);
 	makeSeuss voice4("DrSeuss.txt","Seussout4.txt",false,false);
 //OR
-	makeSeuss voice5("GEChap1a.txt","GEout1.txt",true,true);
-	makeSeuss voice6("GEChap1a.txt","GEout2.txt",false,true);
-	makeSeuss voice7("GEChap1a.txt","GEout3.txt",true,false);
-	makeSeuss voice8("GEChap1a.txt","GEout4.txt",false,false);
+//	makeSeuss voice5("GEChap1a.txt","GEout1.txt",true,true);
+//	makeSeuss voice6("GEChap1a.txt","GEout2.txt",false,true);
+//	makeSeuss voice7("GEChap1a.txt","GEout3.txt",true,false);
+//	makeSeuss voice8("GEChap1a.txt","GEout4.txt",false,false);
 
 	return 0;
 }
